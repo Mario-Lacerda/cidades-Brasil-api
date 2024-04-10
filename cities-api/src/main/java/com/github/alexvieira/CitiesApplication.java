bc5a83441b83625f650150fd35f71d6c94cf3f53
Mário-Lacerda <138284631+Mario-Lacerda@users.noreply.github.com>
2024-04-10 10:21:36 -0300

    Update CitiesApplication.java

--- cities-api/src/main/java/com/github/alexvieira/CitiesApplication.java
+++ cities-api/src/main/java/com/github/alexvieira/CitiesApplication.java
@@ -1,4 +1,4 @@
-package com.github.andrelugomes;
+package com.github.mario lacerda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
